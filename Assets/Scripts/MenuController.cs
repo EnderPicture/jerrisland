e812bb030c7b16818fd69cf38a91680eb54a572f
Donny Wu <enderpicture@gmail.com>
2020-11-01 22:53:21 -0800

    inventory system working

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
+
 public class MenuController : MonoBehaviour
 {
-    List<Transform> inventorySlots = new List<Transform>();
-    List<JournalItem> journalItems  = new List<JournalItem>();
+    public List<Transform> inventorySlots = new List<Transform>();
+    List<JournalItem> journalItems = new List<JournalItem>();
     public Transform inventory;
+    public Transform inventoryRoot;
+    public TextMeshProUGUI selectedItemName;
+    int viewingIndex = 0;
+    JournalItem currentItem;
+    public GameObject selectBG;
     // Start is called before the first frame update
     void Start()
     {
+        DOTween.Init();
         // get all the slots from the inventory
         for (int c = 0; c < inventory.childCount; c++)
         {
@@ -28,12 +36,38 @@ public class MenuController : MonoBehaviour
     {
 
     }
-    private void UpdateMenu()
+    public void show(bool show)
+    {
+        GetComponent<Canvas>().enabled = show;
+    }
+    void UpdateSelect()
+    {
+        currentItem = journalItems.Find(d => d.index == viewingIndex);
+
+        if (currentItem == null)
+        {
+            selectedItemName.text = "not collected yet";
+        }
+        else
+        {
+            selectedItemName.text = currentItem.itemName;
+        }
+        selectBG.transform.DOMove(inventorySlots[viewingIndex].transform.position, 0.2f).SetEase(Ease.InOutQuint);
+    }
+    public void increment(int delta)
+    {
+        viewingIndex += delta;
+        viewingIndex = viewingIndex > inventorySlots.Count - 1 ? 0 : viewingIndex;
+        viewingIndex = viewingIndex < 0 ? inventorySlots.Count - 1 : viewingIndex;
+        UpdateSelect();
+    }
+    void UpdateMenu()
     {
         foreach (JournalItem item in journalItems)
         {
             if (item != null)
             {
+                Debug.Log(inventorySlots.Count);
                 Transform slot = inventorySlots[item.index];
                 TextMeshProUGUI text = slot.GetChild(0).GetComponent<TextMeshProUGUI>();
                 text.text = item.itemName;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerController : MonoBehaviour
 {
     public MenuController menuController;
+    FirstPersonController fpController;
+    bool menuUp = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        fpController = GetComponent<FirstPersonController>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetButtonDown("Inventory"))
+        {
+            menuUp = !menuUp;
+            fpController.enabled = !menuUp;
+            menuController.show(menuUp);
+            if (menuUp) {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            
+        }
     }
     public void JournalItemGain(JournalItem journalItem)
     {
