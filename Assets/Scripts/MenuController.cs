bd52886e688692d8f50ba5a7e61ac9cc10e3c013
Donny Wu <enderpicture@gmail.com>
2020-11-01 21:03:53 -0800

    item collection and menu updator

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class MenuController : MonoBehaviour
 {
+    List<Transform> inventorySlots = new List<Transform>();
+    List<JournalItem> journalItems  = new List<JournalItem>();
+    public Transform inventory;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // get all the slots from the inventory
+        for (int c = 0; c < inventory.childCount; c++)
+        {
+            Transform child = inventory.GetChild(c);
+            for (int j = 0; j < child.childCount; j++)
+            {
+                Transform slot = child.GetChild(j);
+                inventorySlots.Add(slot);
+            }
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void UpdateMenu()
+    {
+        foreach (JournalItem item in journalItems)
+        {
+            if (item != null)
+            {
+                Transform slot = inventorySlots[item.index];
+                TextMeshProUGUI text = slot.GetChild(0).GetComponent<TextMeshProUGUI>();
+                text.text = item.itemName;
+            }
+        }
+    }
+    public void GainJournalItem(JournalItem item)
+    {
+        journalItems.Add(item);
+        UpdateMenu();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public MenuController menuController;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void JournalItemGain(JournalItem journalItem)
+    {
+        menuController.GainJournalItem(journalItem);
     }
 }
