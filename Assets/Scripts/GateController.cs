8708b6f049e3c3e67f52ddbeaaa43a4738b34ed0
Donny Wu <enderpicture@gmail.com>
2020-11-03 21:23:58 -0800

    working on gate controller

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    public int gateID;
+    public GateButton button1;
+    public GateButton button2;
+    public GateButton button3;
+    public GateButton button4;
+    public GatePowerInput powerInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button1.setGateID(gateID);
+        button2.setGateID(gateID);
+        button3.setGateID(gateID);
+        button4.setGateID(gateID);
+        powerInput.setGateID(gateID);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class PlayerController : MonoBehaviour
 {
     public MenuController menuController;
+    public float gravityGunRange;
+    public Transform gravityGunHoldingSpot;
+    Transform ggHolding;
     FirstPersonController fpController;
     bool menuUp = false;
     // Start is called before the first frame update
@@ -17,21 +20,69 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        inventoryToggle();
+        if (ggHolding != null)
+        {
+            ggHolding.GetComponent<Rigidbody>().AddForce(10 * (gravityGunHoldingSpot.position - ggHolding.position));
+        }
+    }
+    void inventoryToggle()
+    {
         if (Input.GetButtonDown("Inventory"))
         {
             menuUp = !menuUp;
             fpController.enabled = !menuUp;
             menuController.show(menuUp);
-            if (menuUp) {
+            if (menuUp)
+            {
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
             }
-            else {
+            else
+            {
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
             }
-            
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GravityGun();
+        }
+    }
+    void GravityGun()
+    {
+
+        if (ggHolding == null)
+        {
+            int layerMask = ~(1 << LayerMask.NameToLayer("Player"));
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, gravityGunRange, layerMask))
+            {
+                if (hit.transform.tag == "Holdable")
+                {
+                    ggHolding = hit.transform;
+
+
+                    Vector3 spotPos = gravityGunHoldingSpot.localPosition;
+                    spotPos.z = transform.InverseTransformPoint(ggHolding.position).z;
+                    gravityGunHoldingSpot.localPosition = spotPos;
+
+                    // ggHolding.parent = gravityGunHoldingSpot;
+                    Rigidbody rb = ggHolding.GetComponent<Rigidbody>();
+                    rb.useGravity = false;
+                    rb.drag = 5;
+                    rb.angularDrag = 5;
+                }
+            }
+        }
+        else
+        {
+            // ggHolding.parent = null;
+            Rigidbody rb = ggHolding.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb.drag = 0;
+            rb.angularDrag = 0.05f;
+            ggHolding = null;
         }
     }
     public void JournalItemGain(JournalItem journalItem)
