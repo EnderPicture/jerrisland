15982915c0671e425ed93dfc0786f07a9dd4bc2d
Donny Wu <enderpicture@gmail.com>
2020-11-06 21:13:05 -0800

    added sound prefab

---
+++ Assets/Scripts/AutoKillSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoKillSound : MonoBehaviour
+{
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
